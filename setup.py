5a227c3b2be8b6068b7eb033705a66617f104e6a
Tyler Treat <ttreat31@gmail.com>
2014-02-24 22:57:09 -0600

    Make it pip installable

--- setup.py
+++ setup.py
@@ -1,16 +1,27 @@
 from setuptools import find_packages
 from setuptools import setup
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 
-setup(
-    name='sautils',
+setup_args = dict(
+    name='SQLAlchemyUtils',
+    description='Useful utilities for working with SQLAlchemy.',
+    url='https://github.com/tylertreat/SQLAlchemyUtils',
     version=VERSION,
+    license='Apache',
     packages=find_packages(),
-    install_requires=[],
     include_package_data=True,
-    zip_safe=False,
-    maintainer='Tyler Treat',
-    maintainer_email='ttreat31@gmail.com'
+    author='Tyler Treat',
+    author_email='ttreat31@gmail.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
 )
+
+if __name__ == '__main__':
+    setup(**setup_args)
 
